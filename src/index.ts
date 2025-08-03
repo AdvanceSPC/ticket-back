aacf18dce9499106f20504da5445d10183163edf
AdvanceSPC <cx@advancespc.com>
2025-08-03 18:01:36 -0500

    Permite Cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { authenticate } from "./middlewares/auth.middlewares";
@@ -6,11 +7,19 @@ import ticketRoutes from "./routes/ticket.routes";
 
 dotenv.config();
 const app = express();
-
+app.use(cors({
+  origin: [
+    "http://localhost:5173", 
+    "http://localhost:3000",
+    "http://localhost:4173"
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/tickets", authenticate, ticketRoutes);
 
 const PORT = process.env.PORT || 3000;
-console.log(process.env.JWT_SECRET)
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
